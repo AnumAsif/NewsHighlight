1f89289cf2ed75593c1495abd741541ea31078df
Anum Asif <anum@cockar.com>
2019-02-03 22:21:16 +0300

    Work on API key to make it work

--- app/requests.py
+++ app/requests.py
@@ -16,7 +16,7 @@ def get_news(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_news_url = base_url.format(category,'2019-02-03',api_key)
+    get_news_url = base_url.format(category,api_key)
     with urllib.request.urlopen(get_news_url) as url:
         get_news_data = url.read()
         get_news_response= json.loads(get_news_data)
